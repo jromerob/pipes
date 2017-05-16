4d13180b7fa25119b2726bc7d350a8523dcf85f7
Juan Carlos <jcromero@viewnext.com>
2017-05-16 18:00:47 +0200

    Cap 71 Pipe JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = 'Juan Carlos';
-  array=["1","2","3","4","5","6","7","8","9"];
-  PI=Math.PI;
-  porcentaje=0.234;
-  importe=1234.5;
+  array = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  PI = Math.PI;
+  porcentaje = 0.234;
+  importe = 1234.5;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: '19'
+    }
+  };
 }
